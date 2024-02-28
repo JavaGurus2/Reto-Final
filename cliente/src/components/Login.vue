1745bc7aecbc413c0b8635598e6ca8d7f3e37fa8
Jorge Aparicio <Jorge.aparicio@ikasle.egibide.org>
2024-02-28 08:19:31 +0100

    Login inicial

---
+++ cliente/src/components/Login.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <form class="row d-flex flex-comlumn">
+    <div class="form-floating col-12">
+      <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+      <label for="email">Email address</label>
+    </div>
+    <div class="col-12 mt-2">
+      <input type="password" placeholder="Contraseña: " />
+    </div>
+  </form>
+</template>
