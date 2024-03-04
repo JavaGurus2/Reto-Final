7580f261cda804bf702074cfdbf8c1ba0e5640be
BeatrizMoran <beatriz.moran@ikasle.egibide.org>
2024-03-04 10:46:59 +0100

    seeder categoria

--- servidor/retoFinal/database/seeders/CategoriasSeeder.php
+++ servidor/retoFinal/database/seeders/CategoriasSeeder.php
@@ -4,14 +4,50 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriasSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        // Categorías para películas
+        $categoriasPeliculas = [
+            'Acción',
+            'Comedia',
+            'Drama',
+            'Ciencia Ficción',
+            'Suspenso',
+            "Terror"
+        ];
+
+        // Insertar las categorías para películas
+        foreach ($categoriasPeliculas as $categoria) {
+            DB::table('categorias')->insert([
+                'nombre' => $categoria,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        // Categorías para series
+        $categoriasSeries = [
+            'Drama',
+            'Comedia',
+            'Fantasía',
+            'Terror',
+            'Acción',
+        ];
+
+        // Insertar las categorías para series
+        foreach ($categoriasSeries as $categoria) {
+            DB::table('categorias')->insert([
+                'nombre' => $categoria,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
