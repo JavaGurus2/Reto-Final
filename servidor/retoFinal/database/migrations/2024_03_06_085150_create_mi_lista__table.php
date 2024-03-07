b69d83ec6d94bd744f2566f538f74af665761b8a
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2024-03-07 10:03:21 +0100

    Corregido la tabla de la migracion de mi lista

--- servidor/retoFinal/database/migrations/2024_03_06_085150_create_mi_lista__table.php
+++ servidor/retoFinal/database/migrations/2024_03_06_085150_create_mi_lista__table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('mi_lista_', function (Blueprint $table) {
+        Schema::create('mi_listas', function (Blueprint $table) {
             $table->id();
             $table->string("tipo");
             $table->unsignedBigInteger("referencia_id");
