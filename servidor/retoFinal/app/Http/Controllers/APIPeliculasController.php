a7eb7015e951c12256f702a7b4bb241ac90e41b7
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2024-03-07 18:04:45 +0100

    Importado User en apipeliculas

--- servidor/retoFinal/app/Http/Controllers/APIPeliculasController.php
+++ servidor/retoFinal/app/Http/Controllers/APIPeliculasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use App\Models\Pelicula;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class APIPeliculasController extends Controller
