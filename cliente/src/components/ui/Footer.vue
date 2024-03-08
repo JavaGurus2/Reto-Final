9794f11248f39ccc91fecc142e34d62c464a137f
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2024-03-08 10:47:55 +0100

    Cerrar

--- cliente/src/components/ui/Footer.vue
+++ cliente/src/components/ui/Footer.vue
@@ -53,7 +53,13 @@ function cerrarSesion() {
       </RouterLink>
     </div>
     <div class="col d-flex justify-content-center">
-      <button @click="cerrarSesion">
+      <button
+        @click="
+          () => {
+            cerrarSesion()
+          }
+        "
+      >
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
           <path
             fill="#ffffff"
